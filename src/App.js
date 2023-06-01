7d9e56851a9c30d9b69a2a1ef0162c2a1855fe85
BlakeBuchholz <blakebuchholz@gmail.com>
2023-05-31 22:47:07 -0500

    Unboofed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  const Addition = () => {
+    setCounter(counter + 1);
+  }
+
+  const Subtraction = () => {
+    setCounter(counter - 1);
+  }
+
+  const Reset = () => {
+    setCounter(counter - counter);
+  }
+
+  const Multiply = () => {
+    setCounter(counter * 2);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="container  my-5 bg-secondary">
+      <div className="card text-center text-light my-5 bg-dark">
+        <div className="card-body">
+          <h1>Counter App</h1>
+          <div className="my-5">
+            <h2 className="my-5">{counter}</h2>
+            <button onClick={Addition} className="btn btn-success mx-3">Increment</button>
+            <button onClick={Multiply} className="btn btn-primary mx-3">Multiply</button>
+            <button onClick={Subtraction} className="btn btn-danger mx-3">Decrement</button>
+            <button onClick={Reset} className="btn btn-secondary mx-3">Reset</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
 
 export default App;
